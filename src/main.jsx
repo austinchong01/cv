be5cc760b48bfb7918bba3be2f24e18f054a5f5e
Austin <austin.inhyuk.chong@gmail.com>
2025-05-20 11:10:04 -0700

    Finish CV

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import "./Cv.css"
-import {Form} from './Cv.jsx'
-import App from './App.jsx'
+import "./styles/index.css"
+import "./styles/Cv.css"
+import {Form} from './components/Cv.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
